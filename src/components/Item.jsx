eb35c14cdfffd62034dd63221717038628379a87
claudiajerome07 <claudiajerome07@gmail.com>
2025-02-03 11:10:09 +0530

    completed assignment

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,14 @@
-const Item = ({ item }) => {
+    const Item = ({ item,onDelete }) => {
 
-    // Render a single item
-    // Add a Delete and Edit button
-    return null;
-};
+        // Render a single item
+        // Add a Delete and Edit button
+        // return null;
+        return(
+            <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px" }}>
+                <span>{item.name}</span>
+                <button onClick={() => onDelete(item.id)}>Delete</button>
+            </div>
+        )
+    };
 
-export default Item;
+    export default Item;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,11 +1,35 @@
 import Item from "./Item";
+import axios from "axios";
+import { useState, useEffect } from "react";
+const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
 
-const ItemList = ({ items }) => {
+const ItemList = () => {
     // your code here
+
+    const [items,setItems]=useState([])
+
+    useEffect(()=>{
+        axios.get(API_URI)
+        .then((res)=>{
+            setItems(res.data)
+        })
+        .catch((err)=>{
+            console.log(err.message)
+        })
+    },[])
+
+    const handleDelete=async(id)=>{
+        try{
+            const response=await axios.delete(`${API_URI}/${id}`)
+            setItems(items.filter(item=>item.id!==id))
+        }catch(err){
+            console.log(err.message)
+        }
+    }
     return (
         <>
             {items.map((item) => (
-                <Item key={item.id} item={item} />
+                <Item key={item.id} item={item} onDelete={handleDelete} />
             ))}
         </>
     );
